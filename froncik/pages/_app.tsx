6d2686ba29d35c9c040e55b6c4306d88b585cf95
KGrzeg <kupczykgrzeg@gmail.com>
2022-10-27 22:44:02 +0200

    Navbar

--- froncik/pages/_app.tsx
+++ froncik/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from "next/head";
+import type { AppProps } from "next/app";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
