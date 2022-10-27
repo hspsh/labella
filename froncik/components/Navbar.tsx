197a5fd3a80339d6b400fc46ecfaafe8d5d199ea
KGrzeg <kupczykgrzeg@gmail.com>
2022-10-27 23:42:30 +0200

    Moved bootstrap layout into components

--- froncik/components/Navbar.tsx
+++ froncik/components/Navbar.tsx
@@ -1,21 +1,30 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import NavbarBs from 'react-bootstrap/Navbar';
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import NavbarBs from "react-bootstrap/Navbar";
 
 export default function Navbar() {
   return (
-    <NavbarBs bg="light" expand="lg">
-      <Container>
-        <NavbarBs.Brand href="#home">Labella</NavbarBs.Brand>
-        <NavbarBs.Toggle aria-controls="basic-navbar-nav" />
-        <NavbarBs.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-end flex-grow-1">
-            <Nav.Link href="#clear" className="text-danger">Wyczyść kolejkę</Nav.Link>
-            <Nav.Link href="#add" className="text-primary">Dodaj szablon</Nav.Link>
-          </Nav>
-        </NavbarBs.Collapse>
-      </Container>
-    </NavbarBs>
-  )
+    <Row>
+      <Col>
+        <NavbarBs bg="light" expand="lg">
+          <Container>
+            <NavbarBs.Brand href="#home">Labella</NavbarBs.Brand>
+            <NavbarBs.Toggle aria-controls="basic-navbar-nav" />
+            <NavbarBs.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto justify-content-end flex-grow-1">
+                <Nav.Link href="#clear" className="text-danger">
+                  Wyczyść kolejkę
+                </Nav.Link>
+                <Nav.Link href="#add" className="text-primary">
+                  Dodaj szablon
+                </Nav.Link>
+              </Nav>
+            </NavbarBs.Collapse>
+          </Container>
+        </NavbarBs>
+      </Col>
+    </Row>
+  );
 }
-
--- froncik/pages/index.tsx
+++ froncik/pages/index.tsx
@@ -1,6 +1,4 @@
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 import Navbar from "../components/Navbar";
 import TemplateCards from "../components/TemplateCards";
@@ -8,16 +6,8 @@ import TemplateCards from "../components/TemplateCards";
 export default function Home() {
   return (
     <Container>
-      <Row>
-        <Col>
-          <Navbar />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <TemplateCards />
-        </Col>
-      </Row>
+      <Navbar />
+      <TemplateCards />
     </Container>
   );
 }
