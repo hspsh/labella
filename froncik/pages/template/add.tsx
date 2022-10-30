d230c3f9e881a3cd560c5c40e7695011284a26c8
KGrzeg <kupczykgrzeg@gmail.com>
2022-10-31 00:17:46 +0100

    Reuse form layout

--- froncik/pages/template/add.tsx
+++ froncik/pages/template/add.tsx
@@ -1,6 +1,5 @@
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { Row, Col, Card } from "react-bootstrap";
 
 import TemplateForm from "../../components/TemplateForm";
 
@@ -12,6 +11,20 @@ export default function add({}: Props) {
       <Row>
         <Col>
           <TemplateForm submitText="Dodaj" />
+        </Col>
+        <Col>
+          <Card
+            border="primary"
+            key="Primary"
+            text="dark"
+            style={{ width: "18rem" }}
+            className="mb-2"
+          >
+            <Card.Img
+              src="https://via.placeholder.com/150x90?text=elo%20mordzie"
+              alt="Card image"
+            />
+          </Card>
         </Col>
       </Row>
     </Container>
