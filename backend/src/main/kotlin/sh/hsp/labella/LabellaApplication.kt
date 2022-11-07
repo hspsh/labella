1b1575d16f88eb154661e209b798e64518083be3
psuwala <oneat@vp.pl>
2022-11-07 18:41:28 +0100

    updated api path + swagger

--- backend/src/main/kotlin/sh/hsp/labella/LabellaApplication.kt
+++ backend/src/main/kotlin/sh/hsp/labella/LabellaApplication.kt
@@ -2,10 +2,12 @@ package sh.hsp.labella
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 
 @SpringBootApplication
+@Import(MainConfiguration::class)
 class LabellaApplication
 
 fun main(args: Array<String>) {
-	runApplication<LabellaApplication>(*args)
+    runApplication<LabellaApplication>(*args)
 }
