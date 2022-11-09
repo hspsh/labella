cbb1a032fe46c70003b8dec3c7717e8da750fa78
KGrzeg <kupczykgrzeg@gmail.com>
2022-11-09 20:55:54 +0100

    Read template attributes from backend

--- froncik/pages/template/print/[id].tsx
+++ froncik/pages/template/print/[id].tsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 import TemplatePrintForm from "../../../components/TemplatePrintForm";
 import FormWithPreview from "../../../components/FormWithPreview";
 
+import API from "../../../lib/api";
+type QueryParams = {
+  id: string;
+};
+
 type Props = {};
 
-export default function print({}: Props) {
-  const form = <TemplatePrintForm variables={["imię", "nick", "meme"]} />;
+export default function Print({}: Props) {
+  const router = useRouter();
+  const id = parseInt((router.query as QueryParams).id);
+  const [attributes, setAttributes] = useState<string[]>([]);
+
+  useEffect(() => {
+    API.templates.attributes(id).then((fields) => {
+      console.log(fields);
+      setAttributes(fields);
+    });
+  }, []);
+
+  const form = <TemplatePrintForm variables={attributes} />;
 
   return <FormWithPreview form={form} />;
 }
