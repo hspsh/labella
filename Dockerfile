13e6b5bfecf2238d68925b26d9a4c76fad17f7b6
psuwala <oneat@vp.pl>
2022-11-09 22:24:39 +0100

    docker fonts

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN ./mvnw package
 
 FROM openjdk:17-jdk-alpine as labella
 RUN apk --update add imagemagick
-RUN apk add terminus-font ttf-inconsolata ttf-dejavu font-bitstream-* font-noto font-noto-cjk ttf-font-awesome font-noto-extra
+RUN apk add terminus-font ttf-inconsolata ttf-dejavu font-noto font-noto-cjk ttf-font-awesome font-noto-extra
 COPY --from=1 target/*.jar app.jar
 VOLUME /data
 ENTRYPOINT ["java","-jar","/app.jar","-Dspring.datasource.url=jdbc:h2:file:/data/labella"]
--- backend/src/main/kotlin/sh/hsp/labella/services/svg/flavor/QRCodeFlavor.kt
+++ backend/src/main/kotlin/sh/hsp/labella/services/svg/flavor/QRCodeFlavor.kt
@@ -12,6 +12,17 @@ class QRCodeFlavor : SVGFlavor {
 
         val nodeList = svgRoot.getElementsByTagName("svg:image")
 
+        for (i in 0 until nodeList.length) {
+            val image = nodeList.item(i)
+            val qrCode = image.attributes.getNamedItem("qrCode")
+            if (qrCode != null) {
+                val width = image.attributes.getNamedItem("width").textContent.toInt()
+                val height = image.attributes.getNamedItem("height").textContent.toInt()
+
+                val generatedQrCode =
+
+            }
+        }
         return ""
     }
 }
