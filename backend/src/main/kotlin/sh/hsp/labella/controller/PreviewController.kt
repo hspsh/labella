ec859371161239318bbd6f1da189ee06097805f7
psuwala <oneat@vp.pl>
2022-11-09 18:43:16 +0100

    preview now taking params from url

--- backend/src/main/kotlin/sh/hsp/labella/controller/PreviewController.kt
+++ backend/src/main/kotlin/sh/hsp/labella/controller/PreviewController.kt
@@ -1,39 +1,17 @@
 package sh.hsp.labella.controller
 
-import org.springframework.data.rest.webmvc.ResourceNotFoundException
 import org.springframework.web.bind.annotation.*
-import sh.hsp.labella.services.renderer.RendererService
-import sh.hsp.labella.services.template.TemplateService
+import sh.hsp.labella.services.printing.LanguagePrintingService
 import java.awt.image.BufferedImage
 
 @RestController
-@CrossOrigin(origins = ["*"] )
+@CrossOrigin(origins = ["*"])
 @RequestMapping(path = ["/api/templates/{templateId}/preview"])
 class PreviewController(
-    val templateService: TemplateService,
-    val rendererService: RendererService,
-    val templateRepository: TemplateRepository
+    val languagePrintingService: LanguagePrintingService
 ) {
     @GetMapping(produces = ["image/png"])
-    fun preview(@PathVariable templateId: Long, @RequestBody printDTO: PreviewDTO): BufferedImage {
-        val maybeTemplate = templateRepository.findById(templateId)
-        if (maybeTemplate.isEmpty) {
-            throw ResourceNotFoundException()
-        }
-
-        val template = maybeTemplate.get()
-
-        val render =
-            template
-                .render(
-                    printDTO.fields,
-                    { content, fields -> templateService.render(content, fields) },
-                    { _ -> null },
-                    { input -> rendererService.render(input) }
-                )
-
-        return render.image
+    fun preview(@PathVariable templateId: Long, @RequestParam fields: Map<String, String>): BufferedImage {
+        return languagePrintingService.preview(templateId, fields).image
     }
 }
-
-data class PreviewDTO(val fields: Map<String, String>) {}
