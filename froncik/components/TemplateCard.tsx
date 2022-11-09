124bb58144408f3f425dcdfa26d26c635fb41887
KGrzeg <kupczykgrzeg@gmail.com>
2022-11-09 20:40:10 +0100

    Fit preview into card

--- froncik/components/TemplateCard.tsx
+++ froncik/components/TemplateCard.tsx
@@ -25,11 +25,15 @@ export default function TemplateCard({ template }: Props) {
     >
       <Card.Header>{template.name}</Card.Header>
       <Stack direction="horizontal">
-        <Card.Img
-          src={API.templates.previewSrc(template.id)}
-          alt="Card image"
-          style={{ "--bs-card-inner-border-radius": 0 } as React.CSSProperties}
-        />
+        <div style={{ display: "inline-table" }}>
+          <Card.Img
+            src={API.templates.previewSrc(template.id)}
+            alt="Card image"
+            style={
+              { "--bs-card-inner-border-radius": 0 } as React.CSSProperties
+            }
+          />
+        </div>
         <Stack>
           <ButtonGroup
             vertical
