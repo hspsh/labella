c897032f3682468f9e9d5810d62a5fba18ff7daa
psuwala <oneat@vp.pl>
2022-11-12 18:00:40 +0100

    redirect

--- froncik/pages/template/add.tsx
+++ froncik/pages/template/add.tsx
@@ -1,16 +1,19 @@
 import TemplateForm from "../../components/TemplateForm";
-import FormWithPreview from "../../components/FormWithPreview";
 
 import API from "../../lib/api";
+import routes from "../../lib/routes";
+import {useRouter} from "next/router";
 
 type Props = {};
 
 export default function Add({}: Props) {
+  const router = useRouter()
   return (
     <TemplateForm
       submitText="Dodaj"
       submitCallback={(template) => {
-        API.templates.create(template.name, template.template, template.type);
+        API.templates.create(template.name, template.template, template.type)
+          .then(() => router.push(routes.home()));
       }}
     />
   );
