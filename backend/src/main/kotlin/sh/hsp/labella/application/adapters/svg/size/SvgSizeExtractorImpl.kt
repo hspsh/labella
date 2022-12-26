77d6e8006b89863dc3e076ff392191ffe107ce37
psuwala <oneat@vp.pl>
2022-12-26 18:01:46 +0100

    xD

--- backend/src/main/kotlin/sh/hsp/labella/application/adapters/svg/size/SvgSizeExtractorImpl.kt
+++ backend/src/main/kotlin/sh/hsp/labella/application/adapters/svg/size/SvgSizeExtractorImpl.kt
@@ -1,15 +1,21 @@
 package sh.hsp.labella.application.adapters.svg.size
 
+import org.w3c.dom.Element
+import org.w3c.dom.NodeList
+import sh.hsp.labella.application.adapters.svg.flavor.RewritingCodeFlavor
 import sh.hsp.labella.model.PrintDimensions
 import sh.hsp.labella.model.ports.SvgSizeExtractor
 import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.xpath.XPathConstants
 
 class SvgSizeExtractorImpl : SvgSizeExtractor {
     override fun extract(svg: String): PrintDimensions? {
         val documentBuilder = DocumentBuilderFactory.newInstance()
         val doc = documentBuilder.newDocumentBuilder().parse(svg.byteInputStream())
 
-        val svgRoot = doc.documentElement
+        val rootSvgs = RewritingCodeFlavor.xPath.compile("//svg")
+        val nodes = rootSvgs.evaluate(doc, XPathConstants.NODESET) as NodeList
+        val svgRoot = nodes.item(0) as Element
 
         return runCatching<PrintDimensions> {
             val width = svgRoot.getAttribute("width").toInt()
